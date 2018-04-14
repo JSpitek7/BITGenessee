6b67741db82edb91fb8d93f859173497f797adb5
JSpitek7 <37716680+JSpitek7@users.noreply.github.com>
2018-04-13 21:10:22 -0400

    Add files via upload

---
+++ BITGenesee/frmlogin.vb
@@ -0,0 +1,35 @@
+﻿Public Class frmlogin
+    Public main As frmVisual
+    Dim userdata As New Database(True)
+
+    ' gets username and password from user, checks them against the users in the database
+    Private Sub btnlogin_click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Try
+            If Not userdata.UserFound(txtUsername.Text, txtPassword.Text) Then
+                Throw New Exception("username/password not found.")
+            Else
+                Me.Hide()
+                main = New frmVisual
+                main.ShowDialog()
+
+
+                Dim result As Integer = MessageBox.Show("are you sure you want to exit a need for genesee solutions?", "exit network solver", MessageBoxButtons.YesNoCancel)
+                If result = DialogResult.Yes Then
+                    Application.Exit()
+                Else
+                    txtPassword.Text = ""
+                    Me.Show()
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "login error")
+        End Try
+    End Sub
+
+    Private Sub btncancel_click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Dim result As Integer = MessageBox.Show("are you sure you want to exit a need for genesee solutions?", "exit pathfinder", MessageBoxButtons.YesNoCancel)
+        If result = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
